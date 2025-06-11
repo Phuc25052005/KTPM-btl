26a9ddbd93d96fcc44e0feca18e5923af26ddb71
Phuc25052005 <147498833+Phuc25052005@users.noreply.github.com>
2025-06-11 21:59:02 +0700

    Update DBConnection.java

--- HotelApp/src/main/java/com/example/hotelloginapp/utils/DBConnection.java
+++ HotelApp/src/main/java/com/example/hotelloginapp/utils/DBConnection.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String SERVER_NAME = "DESKTOP-K1JFJOM"; // Thay thế bằng tên server của bạn
+    private static final String SERVER_NAME = "ADMIN"; // Thay thế bằng tên server của bạn
     private static final String DATABASE_NAME = "HOTEL"; // Thay thế bằng tên database của bạn
     private static final String USERNAME = "sa"; // Thay thế bằng username của bạn
-    private static final String PASSWORD = "1"; // Thay thế bằng password của bạn
+    private static final String PASSWORD = "123456"; // Thay thế bằng password của bạn
 
     private static final String CONNECTION_URL = "jdbc:sqlserver://" + SERVER_NAME + ";databaseName=" + DATABASE_NAME + ";user=" + USERNAME + ";password=" + PASSWORD + ";encrypt=true;trustServerCertificate=true;";
 
